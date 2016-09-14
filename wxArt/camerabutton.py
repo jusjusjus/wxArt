ec1fd1cfed57a553f7a3ef560d06d014fad8b215
bluens <bluens@pks.mpg.de>
2016-09-14 14:45:53 +0200

    Change content image to working camera button.
    wxArt/frame.py
    - minor changes to change Image to CameraButton
    
    wxArt/camerabutton.py
    - new camerabutton class
    - does not rescale camera feed

---
+++ wxArt/camerabutton.py
@@ -0,0 +1,75 @@
+#!/usr/env python
+# ==============================================================================
+# camerabutton.py
+#
+# Purpose:
+# define CameraButton class that is a button and video frame
+#
+# ==============================================================================
+#
+import wx
+import cv, cv2
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# % CameraButton class
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+class CameraButton(wx.BitmapButton):
+
+    def __init__(self, fps, *args, **kwargs):
+        super(CameraButton, self).__init__(*args, **kwargs)
+
+        assert fps<=30, 'CameraButton: frame rate too high, too high...'
+
+        # generate capture object and start reading camera buffer
+        self.capture = cv2.VideoCapture(0)
+        ret, cam_frame = self.capture.read()
+        cam_frame = cv2.cvtColor(cam_frame, cv2.COLOR_BGR2RGB)
+
+        # make bitmap from buffer
+        height, width = cam_frame.shape[:2]
+        self.bmp = wx.BitmapFromBuffer(width, height, cam_frame) # buffer for bitmap
+
+        # link bitmap with image on buffer
+        self.SetBitmap(self.bmp)
+
+        # start timer to control redraw
+        self.timer = wx.Timer(self)
+        self.timer.Start(1000./fps)
+
+        # bindings to redraw
+        # self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_TIMER, self.NextCam_Frame)
+
+    # def OnPaint(self, evt):
+    #     '''
+    #     when frame is painted, repaint button
+    #     necessary???
+    #     I thought repaint is controlled from outside
+    #     does work without this method
+    #     '''
+    #     dc = wx.BufferedPaintDC(self)
+    #     dc.DrawBitmap(self.bmp, 0, 0)
+
+    def NextCam_Frame(self, event):
+        '''
+        when time is right get new snapshot from camera
+        and update the bitmap
+        '''
+        ret, cam_frame = self.capture.read()
+        if ret:
+            cam_frame = cv2.cvtColor(cam_frame, cv2.COLOR_BGR2RGB)
+            self.bmp.CopyFromBuffer(cam_frame)
+            self.Refresh()
+
+    def FitBitmap(self):
+        '''
+        TODO
+        does not work properly...
+        '''
+        image = wx.ImageFromBitmap(self.bmp_dummy)
+        Bwidth, Bheight = self.GetClientSize()
+        print Bwidth, Bheight
+        image = image.Scale(Bwidth, Bheight)
+        return wx.BitmapFromImage(image)
+
+
